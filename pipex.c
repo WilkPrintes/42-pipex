227c04164d67069e01727c5dac3be30b61a119b5
WilkPrintes <wilkp90@gmail.com>
2022-02-23 06:15:08 +0100

    infile and outfile added

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: wprintes < wprintes@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 22:39:41 by wprintes          #+#    #+#             */
-/*   Updated: 2022/02/23 04:26:09 by wprintes         ###   ########.fr       */
+/*   Updated: 2022/02/23 06:12:53 by wprintes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,46 +17,73 @@ int main(int argc, char *argv[])
 	char	*cmd;
 	char	**argV;
 	char	*path;
-	int		pid;
-	int		status;
-	int		file;
-	int		file2;
-	int		*fd;
+	int *fd;
+	int pid1;
+	int pid2;
+	int file;
+	int file2;
 
-	fd = malloc (sizeof (int) * 2);
-	fd[0] = 0;
-	fd[1] = 0;
-	if (pipe(fd) == -1)
-	{
-		printf("Erro in Pipe\n");
-		return (1);
-	}
-	pid = fork();
-	file = open("ping_resutls.txt", O_WRONLY | O_CREAT, 0777);
-	if (file == -1)
-		return (1); 
-	file2 = dup2(file, STDOUT_FILENO);
-	close(file);
-	argV = ft_split(argv[1], ' ');
-	if (argc <= 1)
+	if (argc != 5)
 	{
 		printf("numero de argumentos inválidos :/\n");
 		return (1);
 	}
-	path = ft_strjoin("","/usr/bin/");
-	cmd = ft_strjoin(path, argV[0]);
-	if (pid == 0)
+	file = open(argv[1], O_RDWR);
+	if (file == -1)
 	{
-		if (execve(cmd, argV, NULL) == -1)
-			perror("Nope\n");
+		perror(argv[4]);
+		return(1);
 	}
-	else
+	file2 = open(argv[4], O_RDWR);
+	if (file2 == -1)
 	{
-		waitpid(pid, &status, 0);
+		perror(argv[4]);			
+		return(1);
+	}
+	dup2(fd[0], STDIN_FILENO);
+	path = ft_strjoin("","/usr/bin/");
+	fd = malloc (sizeof(int) * 2);
+	fd[0] = 0;
+	fd[1] = 0;
+	if (pipe(fd) == -1)
+		return (1);
+	pid1 = fork();
+	if (pid1 == -1)
+		return(2);
+	if (pid1 == 0)
+	{
 		argV = ft_split(argv[2], ' ');
 		cmd = ft_strjoin(path, argV[0]);
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[0]);
+		close(fd[1]);
 		if (execve(cmd, argV, NULL) == -1)
-			perror("Nope\n");
-		free(argV);
+		{
+			dup2(file2, STDOUT_FILENO);
+			perror(cmd);
+			return(1);
+		}
 	}
+	pid2 = fork();
+	if (pid2 == -1)
+	{
+		return (3);
+	}
+	if (pid2 == 0)
+	{
+		close(fd[0]);
+		close(fd[1]);
+		dup2(file2, STDOUT_FILENO);
+		argV = ft_split(argv[3], ' ');
+		cmd = ft_strjoin(path, argV[0]);
+		if (execve(cmd, argV, NULL) == -1)
+		{
+			perror(cmd);
+			return(1);
+		}
+	}
+	close(fd[0]);
+	close(fd[1]);
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
 }
